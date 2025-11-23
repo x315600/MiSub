1295227f05a2e46af5324d82ac943556510d6cf3
ccnuzw <ccnuzw@gmail.com>
2025-11-23 21:08:35 +0800

    增加订阅在线预览功能2

--- vite.config.js
+++ vite.config.js
@@ -101,10 +101,6 @@ export default defineConfig({
         target: 'http://127.0.0.1:8787',
         changeOrigin: true,
       },
-      '/proxy': {
-        target: 'http://127.0.0.1:8787',
-        changeOrigin: true,
-      }
     }
   }
 })

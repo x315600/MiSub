c212d60a7ed9f7171edd9ac9869d914abee19039
ccnuzw <ccnuzw@gmail.com>
2026-01-07 11:41:44 +0800

    chore: 更新JSON模块导入语法从`assert`到`with`

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
-import manifest from './public/manifest.json' assert { type: 'json' }
+import manifest from './public/manifest.json' with { type: 'json' }
 
 export default defineConfig({
   plugins: [
